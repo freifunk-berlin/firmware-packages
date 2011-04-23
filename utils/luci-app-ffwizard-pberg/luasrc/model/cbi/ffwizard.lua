8f914d67b3cf0efcd0a296f420df57b95d34f73f
Patrick Grimm <patrick@lunatiki.de>
2011-04-23 20:26:52 +0200

    [ffwizard] fix has_hostapd

--- utils/luci-app-ffwizard-pberg/luasrc/model/cbi/ffwizard.lua
+++ utils/luci-app-ffwizard-pberg/luasrc/model/cbi/ffwizard.lua
@@ -33,7 +33,7 @@ local has_autoipv6  = fs.access("/usr/bin/auto-ipv6")
 local has_qos  = fs.access("/etc/init.d/qos")
 local has_ipv6 = fs.access("/proc/sys/net/ipv6")
 local has_hb = fs.access("/sbin/heartbeat")
-local has_wpad = fs.access("/usr/sbin/hostapd")
+local has_hostapd = fs.access("/usr/sbin/hostapd")
 
 luci.i18n.loadc("freifunk")
 
@@ -307,7 +307,7 @@ uci:foreach("wireless", "wifi-device",
 				uci:save("freifunk")
 			end
 		local hwtype = section.type
-		if hwtype == "atheros" or ( hwtype == "mac80211" and has_wpad ) then
+		if hwtype == "atheros" or ( hwtype == "mac80211" and has_hostapd ) then
 			local vap = f:field(Flag, "vap_" .. device , "Virtueller Drahtloser Zugangspunkt", "Konfigurieren Sie Ihren Virtuellen AP")
 			vap:depends("client_" .. device, "1")
 			vap.rmempty = false
