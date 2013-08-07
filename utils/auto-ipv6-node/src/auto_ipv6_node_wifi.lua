672a9845fa969f4b35d28c69d640dcecc24e1792
Patrick Grimm <patrick@lunatiki.de>
2013-08-07 20:51:14 +0200

    [auto-ipv6] rewrite

--- utils/auto-ipv6-node/src/auto_ipv6_node_wifi.lua
+++ utils/auto-ipv6-node/src/auto_ipv6_node_wifi.lua
@@ -32,6 +32,7 @@ end
 function get_profiles()
 	local list = fs.glob(profiles .. "*") or {}
 	local pft = {}
+
 	for k,v in ipairs(list) do
 		pft[#pft+1] = {}
 		local n = string.gsub(v, profiles, "")
@@ -41,11 +42,13 @@ function get_profiles()
 		pft[#pft].bssid = string.upper(uci:get_first("profile_"..n, "defaults", "bssid") or "")
 		pft[#pft].channel = uci:get_first("profile_"..n, "defaults", "channel") or ""
 	end
+
 	return pft
 end
 
 function get_wconf(tbl,pft)
 	local wconf = {}
+
 	for i, net in ipairs(tbl) do
 		if net.mode == "Ad-Hoc" then
 			print(net.quality,net.bssid,net.channel,net.signal.." dB",net.quality,net.quality_max.." max")
@@ -82,120 +85,172 @@ function get_wconf(tbl,pft)
 --					print("bssid profile %x%x:[] "..profile.uciname,"profile"..profile.bssid,"net "..net.bssid)
 --				end
 			end
+
 			return  wconf
 		end
 	end
+
 	return wconf
 end
+
 
 
 local profiles = get_profiles()
 ntm.init(uci)
 local devices = ntm:get_wifidevs()
 local dev
-for _,dev in ipairs(devices) do
-	dev:set("disabled", nil)
-	for _, net in ipairs(dev:get_wifinets()) do
-		print(dev:name())
-		local ifc = net:get_interface()
-		print(ifc.ifname)
-		local scan = iwscanlist(ifc.ifname,3)
-		table.sort(scan, function(a, b) 
-			return tonumber(a["quality"]) > tonumber(b["quality"])
-		end)
-		local wificonfig = get_wconf(scan,profiles)
-		print(wificonfig.profile,wificonfig.ssid,wificonfig.bssid,wificonfig.channel)
-		external = "profile_"..wificonfig.profile
-		dev:set("channel", wificonfig.channel)
-		dev:set("noscan",true)
-		dev:set("distance",1000)
-		net:set("ssid",wificonfig.ssid)
-		net:set("bssid",wificonfig.bssid)
-		net:set("mode",wificonfig.mode or "adhoc")
-		net:set("network","wireless0")
-		ntm:save("wireless")
-		ntm:commit("wireless")
+local profile_name
+local external
 
-		uci:section("network","interface","wireless0", {
-			proto = "static",
-			ip6assign = 64
-		})
-		uci:set_list("network","wireless0","dns", { "2002:d596:2a92:1:71:53::", "2002:5968:c28e::53" })
-		uci:save("network")
-		uci:commit("network")
+--Add LAN to 6relayd
+local rifn = {}
+table.insert(rifn,"lan")
+uci:set_list("6relayd","default","network",rifn)
+uci:save("6relayd")
 
-		--local rifn = uci:get_list("6relayd","default","network") or {}
-		local rifn = {}
-		table.insert(rifn,"wireless0")
-		table.insert(rifn,"lan")
-		uci:set_list("6relayd","default","network",rifn)
-		uci:save("6relayd")
-		uci:commit("6relayd")
+--Delete all olsrd Interfaces
+uci:delete_all("olsrd", "Interface")
 
-		uci:set("uhttpd", "main", "listen_http", 80)
-		uci:set("uhttpd", "main", "listen_https", 443)
-		uci:save("uhttpd")
-		uci:commit("uhttpd")
-
-		local community = uci:get_all(external, "profile")
-		uci:tset("freifunk", "community", community)
-		uci:set("freifunk", "community", "name", wificonfig.profile)
-		uci:save("freifunk")
-		uci:commit("freifunk")
-
-		local rand = sys.exec("head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1-4")
-		local new_hostname = "OpenWrt-"..rand
-		uci:foreach("system", "system",
-			function(s)
-				-- Make set timzone and zonename
-				uci:set("system", s[".name"], "zonename", "Europe/Berlin")
-				uci:set("system", s[".name"], "timezone", 'CET-1CEST,M3.5.0,M10.5.0/3')
-				-- Set hostname
-				uci:set("system", s[".name"], "hostname", new_hostname)
-				sys.hostname(new_hostname)
-				-- Set hostname
-				uci:set("system", s[".name"], "latitude", community.latitude)
-				uci:set("system", s[".name"], "longitude", community.longitude)
+for i,dev in ipairs(devices) do
+	seq = i - 1
+	print(dev:get("disabled"))
+	if dev:get("disabled") == "1" then
+		dev:set("disabled", "0")
+		for _, net in ipairs(dev:get_wifinets()) do
+			local ifc = net:get_interface()
+			local scan = iwscanlist(ifc.ifname,3)
+			table.sort(scan, function(a, b) 
+				return tonumber(a["quality"]) > tonumber(b["quality"])
 			end)
-		uci:save("system")
-		uci:commit("system")
+			local wificonfig = get_wconf(scan,profiles)
+			profile_name = wificonfig.profile
+			print(wificonfig.profile,wificonfig.ssid,wificonfig.bssid,wificonfig.channel)
+			external = "profile_"..profile_name
+			dev:set("channel", wificonfig.channel)
+			dev:set("noscan",true)
+			dev:set("distance",1000)
+			net:set("ssid",wificonfig.ssid)
+			net:set("bssid",wificonfig.bssid)
+			net:set("mode",wificonfig.mode or "adhoc")
+			net:set("network","wireless"..seq)
+			ntm:save("wireless")
+	
+			uci:section("network","interface","wireless"..seq, {
+				proto = "static",
+				ip6assign = 64
+			})
+			uci:set_list("network","wireless"..seq,"dns", { "2002:d596:2a92:1:71:53::", "2002:5968:c28e::53" })
+			uci:save("network")
+	
+			local rifn = uci:get_list("6relayd","default","network") or {}
+			table.insert(rifn,"wireless"..seq)
+			uci:set_list("6relayd","default","network",rifn)
+			uci:save("6relayd")
 
-		uci:delete_all("olsrd", "olsrd")
-		local olsrbase = uci:get_all("freifunk", "olsrd") or {}
-		utl.update(olsrbase, uci:get_all(external, "olsrd") or {})
-		olsrbase.IpVersion = "6"
-		olsrbase.LinkQualityAlgorithm = "etx_ffeth"
-		olsrbase.NatThreshold = nil
-		uci:section("olsrd", "olsrd", nil, olsrbase)
-		local olsrifbase = uci:get_all("freifunk", "olsr_interface") or {}
-		utl.update(olsrifbase, uci:get_all(external, "olsr_interface") or {})
-		olsrifbase.Ip4Broadcast = nil
-		uci:section("olsrd", "InterfaceDefaults", nil, olsrifbase)
-		uci:delete_all("olsrd", "LoadPlugin", {library="olsrd_jsoninfo.so.0.0"})
-		uci:section("olsrd", "LoadPlugin", nil, {
-			library = "olsrd_jsoninfo.so.0.0",
-			ignore = 0,
-			accept = "::"
-		})
-		uci:delete_all("olsrd", "Interface")
-		local olsrifbase = {}
-		olsrifbase.interface = "wireless0"
-		olsrifbase.ignore = "0"
-		uci:section("olsrd", "Interface", nil, olsrifbase)
-		local olsrifbase = {}
-		olsrifbase.interface = "lan"
-		olsrifbase.ignore = "0"
-		uci:section("olsrd", "Interface", nil, olsrifbase)
-		uci:save("olsrd")
-		uci:commit("olsrd")
-		ready = true
+			local olsrifbase = {}
+			olsrifbase.interface = "wireless"..seq
+			olsrifbase.ignore = "0"
+			uci:section("olsrd", "Interface", nil, olsrifbase)
+			uci:save("olsrd")
+			ready = true
+		end
 	end
 end
 
 if ready then
+
+	--save network
+	uci:commit("network")
+
+	--save wireless
+	ntm:commit("wireless")
+
+	--uhttpd is listen on ipv6
+	uci:set("uhttpd", "main", "listen_http", 80)
+	uci:set("uhttpd", "main", "listen_https", 443)
+	uci:save("uhttpd")
+	uci:commit("uhttpd")
+
+	--set community profile
+	local community = uci:get_all(external, "profile")
+	uci:tset("freifunk", "community", community)
+	uci:set("freifunk", "community", "name", profile_name)
+
+	--save freifunk
+	uci:save("freifunk")
+	uci:commit("freifunk")
+
+	--set olsrd base config
+	uci:delete_all("olsrd", "olsrd")
+	local olsrbase = uci:get_all("freifunk", "olsrd") or {}
+	utl.update(olsrbase, uci:get_all(external, "olsrd") or {})
+	olsrbase.IpVersion = "6"
+	olsrbase.LinkQualityAlgorithm = "etx_ffeth"
+	olsrbase.NatThreshold = nil
+	uci:section("olsrd", "olsrd", nil, olsrbase)
+
+	--set olsrd interface defaults
+	local olsrifbase = uci:get_all("freifunk", "olsr_interface") or {}
+	utl.update(olsrifbase, uci:get_all(external, "olsr_interface") or {})
+	olsrifbase.Ip4Broadcast = nil
+	uci:section("olsrd", "InterfaceDefaults", nil, olsrifbase)
+	uci:delete_all("olsrd", "LoadPlugin", {library="olsrd_jsoninfo.so.0.0"})
+
+	--set olsrd jsoninfo listen on ipv6
+	uci:section("olsrd", "LoadPlugin", nil, {
+		library = "olsrd_jsoninfo.so.0.0",
+		ignore = 0,
+		accept = "::"
+	})
+
+	--add lan to olsr interfaces
+	local olsrifbase = {}
+	olsrifbase.interface = "lan"
+	olsrifbase.ignore = "0"
+	uci:section("olsrd", "Interface", nil, olsrifbase)
+
+	--save olsrd
+	uci:save("olsrd")
+	uci:commit("olsrd")
+
+	local rand = sys.exec("head -n 1 /dev/urandom 2>/dev/null | md5sum | cut -b 1-4")
+	local new_hostname = "OpenWrt-"..rand
+	uci:foreach("system", "system",
+		function(s)
+			-- Make set timzone and zonename
+			uci:set("system", s[".name"], "zonename", "Europe/Berlin")
+			uci:set("system", s[".name"], "timezone", 'CET-1CEST,M3.5.0,M10.5.0/3')
+			-- Set uniq hostname
+			uci:set("system", s[".name"], "hostname", new_hostname)
+			sys.hostname(new_hostname)
+			-- Set hostname
+			uci:set("system", s[".name"], "latitude", community.latitude)
+			uci:set("system", s[".name"], "longitude", community.longitude)
+		end)
+
+	--save system
+	uci:save("system")
+	uci:commit("system")
+
+	--save system
+	uci:commit("6relayd")
+
+	--restart deamons
 	luci.sys.call("(/etc/init.d/network restart) >/dev/null 2>/dev/null")
 	luci.sys.call("(/etc/init.d/olsrd restart) >/dev/null 2>/dev/null")
 	luci.sys.call("(/etc/init.d/6relayd restart) >/dev/null 2>/dev/null")
 	luci.sys.call("(/etc/init.d/uhttpd restart) >/dev/null 2>/dev/null")
+
+else
+
+	--revert saved configs
+	uci:revert("network")
+	uci:revert("wireless")
+	uci:revert("uhttpd")
+	uci:revert("freifunk")
+	uci:revert("olsrd")
+	uci:revert("system")
+	uci:revert("6relayd")
+
 end
 
