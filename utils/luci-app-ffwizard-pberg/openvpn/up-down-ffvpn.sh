7fda64fc8c59ec382505b8fd39be64f978f71f45
Patrick Grimm <patrick@lunatiki.de>
2013-08-22 21:30:28 +0200

    [ffwizard] up-down-ffvpn.sh fix typo

--- utils/luci-app-ffwizard-pberg/openvpn/up-down-ffvpn.sh
+++ utils/luci-app-ffwizard-pberg/openvpn/up-down-ffvpn.sh
@@ -11,7 +11,7 @@ remote=$5
 		gw=$(ip route show dev $dev | grep 0.0.0.0/1 | cut -d ' ' -f 3)
 		logger -t up-down-ffvpn "sleep 5 dev: $dev remote: $remote gw: $gw"
 		sysctl -w net.ipv6.conf.$dev.disable_ipv6=1
-		ip route add default via $gw dev $dev table=111
+		ip route add default via $gw dev $dev table 111
 		ip route del 0.0.0.0/1 via $gw dev $dev
 		ip route del 128.0.0.0/1 via $gw dev $dev
 	fi
