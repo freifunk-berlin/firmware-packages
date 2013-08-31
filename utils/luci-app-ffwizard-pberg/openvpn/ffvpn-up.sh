1fb7d01ae01d55eeeb6890e7408fc1fe9d787e80
Patrick Grimm <patrick@lunatiki.de>
2013-08-31 02:09:40 +0200

    [ffwizard] ffvpn-up rewrite

--- utils/luci-app-ffwizard-pberg/openvpn/ffvpn-up.sh
+++ utils/luci-app-ffwizard-pberg/openvpn/ffvpn-up.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
-logger -t up-down-ffvpn "dev: $1 mtu: $2 local: $4 remote: $5 init: $6"
-
-table=$(uci -q get olsrd.@olsrd[0].RtTable)
-#table=111
-dev=$1
-remote=$5
+table=$(uci -q get olsrd.@olsrd[0].RtTableDefault)
+dev="$1"
+remote="$5"
 
 (
 	sysctl -w net.ipv6.conf.$dev.disable_ipv6=1
 	if [ -n "$table" ] ; then
 		sleep 2
-		gw=$(ip route show dev $dev | grep 0.0.0.0/1 | cut -d ' ' -f 3)
-		logger -t up-down-ffvpn "sleep 2 dev: $dev remote: $remote gw: $gw"
+		ugw="$route_net_gateway"
+		gw="$route_vpn_gateway"
+		src="$ifconfig_local"
+		mask="$ifconfig_netmask"
+		eval $(ipcalc.sh $src $mask)
+		net="$NETWORK/$PREFIX"
+		logger -t up-down-ffvpn "ugw: $ugw dev: $dev remote: $remote gw: $gw src: $src mask: $mask table: $table"
+		ip route add $net dev $dev src $src table $table
+		ip route add $remote_1 via $ugw table main
+		ip route add default via $ugw table default
 		ip route del 0.0.0.0/1 via $gw dev $dev
 		ip route del 128.0.0.0/1 via $gw dev $dev
-		net=$(ip route show dev $dev | cut -d ' ' -f 1)
-		src=$(ip route show dev $dev | cut -d ' ' -f 6)
-		ip route add $net dev $dev src $src table $table
-		ip route add default via $gw dev $dev table $table
-		ip route del $net dev $dev
+		ip route add default via $gw dev $dev table $table metric 10
+		ip rule add pref 20000 iif $dev lookup $table
+		ip rule add pref 20001 iif $dev unreachable
 	fi
 ) >/tmp/ffvpn-up.log 2>&1 &
 
 exit 0
-
