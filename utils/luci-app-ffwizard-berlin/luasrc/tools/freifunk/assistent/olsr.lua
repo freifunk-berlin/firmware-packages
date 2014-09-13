5296831ce2bf74e4d20c9ef90891fc5204040205
Nico von Geyso <nico.geyso@fu-berlin.de>
2014-09-14 00:45:10 +0200

    ffwizard-berlin: enable olsr dyngw plugin by default

--- utils/luci-app-ffwizard-berlin/luasrc/tools/freifunk/assistent/olsr.lua
+++ utils/luci-app-ffwizard-berlin/luasrc/tools/freifunk/assistent/olsr.lua
@@ -15,7 +15,6 @@ function prepareOLSR()
 	uci:delete_all("olsrd", "Hna4")
 	uci:delete_all("olsrd", "Hna6")
 	uci:delete_all("olsrd", "LoadPlugin", {library="olsrd_dyn_gw.so.0.5"})
-	uci:delete_all("olsrd", "LoadPlugin", {library="olsrd_dyn_gw_plain.so.0.4"})
 
 	uci:delete_all("olsrd6", "olsrd")
 	uci:delete_all("olsrd6", "InterfaceDefaults")
@@ -23,7 +22,6 @@ function prepareOLSR()
 	uci:delete_all("olsrd6", "Hna4")
 	uci:delete_all("olsrd6", "Hna6")
 	uci:delete_all("olsrd6", "LoadPlugin", {library="olsrd_dyn_gw.so.0.5"})
-	uci:delete_all("olsrd6", "LoadPlugin", {library="olsrd_dyn_gw_plain.so.0.4"})
 
 	uci:save("olsrd")
 	uci:save("olsrd6")
@@ -61,22 +59,6 @@ function configureOLSR()
 end
 
 function configureOLSRPlugins()
-	if (sharenet == "1") then
-		tools.logger("enable olsr plugin dyn_gw_plain")
-		uci:section("olsrd", "LoadPlugin", nil, {library="olsrd_dyn_gw_plain.so.0.4"})
-		uci:section("olsrd6", "LoadPlugin", nil, {library="olsrd_dyn_gw_plain.so.0.4"})
-	else
-		-- Disable gateway_plain plugin
-		tools.logger("disable olsr plugin dyn_gw_plain")
-		uci:section("olsrd", "LoadPlugin", nil, {
-        		library = "olsrd_dyn_gw_plain.so.0.4",
-			ignore = 1
-		})
-		uci:section("olsrd6", "LoadPlugin", nil, {
-			library = "olsrd_dyn_gw_plain.so.0.4",
-			ignore = 1
-		})
-	end
 	local suffix = uci:get_first(community, "community", "suffix") or "olsr"
 	updatePlugin("olsrd_nameservice.so.0.3", "suffix", "."..suffix)
 	uci:save("olsrd")
