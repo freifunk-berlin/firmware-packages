3a545b7da00dd95481a79c9c067d0a304601b910
Nico Geyso <nico.geyso@fu-berlin.de>
2014-09-21 12:51:02 +0200

    ffwizard-berlin: remove firewall defaults

--- utils/luci-app-ffwizard-berlin/luasrc/tools/freifunk/assistent/firewall.lua
+++ utils/luci-app-ffwizard-berlin/luasrc/tools/freifunk/assistent/firewall.lua
@@ -6,142 +6,42 @@ module ("luci.tools.freifunk.assistent.firewall", package.seeall)
 
 function prepareFirewall()
 	local c = uci.cursor()
-	c:delete_all("firewall","zone", {name="freifunk"})
-	c:delete_all("firewall","forwarding", {dest="freifunk"})
-	c:delete_all("firewall","forwarding", {src="freifunk"})
-	c:delete_all("firewall","rule", {dest="freifunk"})
-	c:delete_all("firewall","rule", {src="freifunk"})
-	c:save("firewall")
-
-	local newzone = tools.firewall_create_zone("freifunk", "ACCEPT", "ACCEPT", "REJECT", 1)
 	local community = "profile"..c:get("freifunk","community","name")
-	if newzone then
-		tools.firewall_zone_add_masq_src("freifunk", "255.255.255.255/32")
-		c:foreach("freifunk", "fw_forwarding", function(section)
-			c:section("firewall", "forwarding", nil, section)
-		end)
-		c:foreach(community, "fw_forwarding", function(section)
-			c:section("firewall", "forwarding", nil, section)
-		end)
 
-		c:foreach("freifunk", "fw_rule", function(section)
-			c:section("firewall", "rule", nil, section)
-		end)
-		c:foreach(community, "fw_rule", function(section)
-			c:section("firewall", "rule", nil, section)
-		end)
-	end
+	c:foreach("freifunk", "fw_forwarding", function(section)
+		c:section("firewall", "forwarding", nil, section)
+	end)
+	c:foreach(community, "fw_forwarding", function(section)
+		c:section("firewall", "forwarding", nil, section)
+	end)
+
+	c:foreach("freifunk", "fw_rule", function(section)
+		c:section("firewall", "rule", nil, section)
+	end)
+	c:foreach(community, "fw_rule", function(section)
+		c:section("firewall", "rule", nil, section)
+	end)
 
 	c:save("firewall")
 end
 
 
 function configureFirewall()
-	tools.firewall_zone_add_interface("freifunk", "dhcp")
-	        uci:delete_all("firewall", "rule", {
-        	src="freifunk",
-  	      	proto="udp",
-        	dest_port="53"
-	})
-	uci:section("firewall", "rule", nil, {
-        	src="freifunk",
-	        proto="udp",
-        	dest_port="53",
-	        target="ACCEPT"
-	})
-	uci:delete_all("firewall", "rule", {
-        	src="freifunk",
-      	  	proto="udp",
-        	src_port="68",
-        	dest_port="67"
-	})
-	uci:section("firewall", "rule", nil, {
-        	src="freifunk",
-	        proto="udp",
-        	src_port="68",
-	        dest_port="67",
-        	target="ACCEPT"
-	})
-	uci:delete_all("firewall", "rule", {
-        	src="freifunk",
-	        proto="tcp",
-        	dest_port="8082",
-	})
-	uci:section("firewall", "rule", nil, {
-        	src="freifunk",
-	        proto="tcp",
-        	dest_port="8082",
-	        target="ACCEPT"
-	})
-	uci:foreach("firewall", "defaults",
-        	function(section)
-                	uci:set("firewall", section[".name"], "drop_invalid", "0")
-        	end)
-
-	local has_advanced = false
-	uci:foreach("firewall", "advanced",
-        	function(section) has_advanced = true end)
-	if not has_advanced then
-        	uci:section("firewall", "advanced", nil,
-               	{ tcp_ecn = "0", ip_conntrack_max = "8192", tcp_westwood = "1" })
-	end
-
-
 	if (sharenet == "1") then
-		uci:delete_all("firewall","zone", {name="wan"})
-		uci:section("firewall", "zone", nil, {
-			masq	= "1",
-			input = "REJECT",
-			forward = "REJECT",
-			name = "wan",
-			output = "ACCEPT",
-			network = "wan"
-		})
-		uci:delete_all("firewall","forwarding", {src="freifunk", dest="wan"})
-		uci:section("firewall", "forwarding", nil, {src="freifunk", dest="wan"})
-		uci:delete_all("firewall","forwarding", {src="wan", dest="freifunk"})
-		uci:section("firewall", "forwarding", nil, {src="wan", dest="freifunk"})
-		uci:delete_all("firewall","forwarding", {src="lan", dest="wan"})
-		uci:section("firewall", "forwarding", nil, {src="lan", dest="wan"})
-		uci:foreach("firewall", "zone",
-			function(s)
-				if s.name == "wan" then
-					uci:set("firewall", s['.name'], "local_restrict", "1")
-					uci:set("firewall", s['.name'], "masq", "1")
-					return false
-				end
-			end)
-		uci:foreach("firewall", "zone",
-			function(s)
-				if s.name == "wan" then
-					uci:set("firewall", s['.name'], "input", "ACCEPT")
-					return false
-				end
-			end)
 		tools.firewall_zone_add_interface("freifunk", "tunl0")
 		tools.firewall_zone_add_interface("freifunk", "ffvpn")
 		ovpn_server_list = uci:get_list("openvpn","ffvpn","remote")
 		for i,v in ipairs(ovpn_server_list) do
 			uci:section("firewall", "rule", nil, {
-				--name="Reject-VPN-over-ff",
+				name="Reject-VPN-over-ff-"..i,
 				dest="freifunk",
 				family="ipv4",
 				proto="udp",
 				dest_ip=v,
-				--dest_port="1194",
 				target="REJECT"
 			})
 		end
-	else
-		uci:delete_all("firewall", "forwarding", {src="freifunk", dest="wan"})
-		uci:foreach("firewall", "zone",
-       		 	function(s)
-                		if s.name == "wan" then
-                        		uci:delete("firewall", s['.name'], "local_restrict")
-                        		return false
-                		end
-			end)
 	end
-
+	uci:delete_all("firewall", "forwarding", {src="freifunk", dest="wan"})
 	uci:save("firewall")
 end
