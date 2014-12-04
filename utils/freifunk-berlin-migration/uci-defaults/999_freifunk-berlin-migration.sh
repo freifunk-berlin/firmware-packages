c3c58fa69b5a70ef26c4a49c6594179122b3091e
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-12-04 20:47:27 +0100

    freifunk-berlin-migration: refactor migration script
    This should contain all migration steps for 0.1.0:
    
    * dhcp leasetime
    * htmode change to HT20
    * openvpn remote dns change

--- utils/freifunk-berlin-migration/uci-defaults/999_freifunk-berlin-migration.sh
+++ utils/freifunk-berlin-migration/uci-defaults/999_freifunk-berlin-migration.sh
@@ -1,42 +1,61 @@
 #!/usr/bin/env sh
 
-source ./lib/functions/semver.sh
+source /lib/functions/semver.sh
 
-#OLD_VERSION=$(uci get system.version.version)
-OLD_VERSION='0.0.0'
-VERSION='0.1.0' # $(cat /etc/openwrt_version)
+OLD_VERSION=$(uci get system.@system[0].version)
+OLD_VERSION=${OLD_VERSION:-'0.0.0'}
+VERSION=$(cat /etc/openwrt_version)
 
-function update_openvpn_remote_config() {
+log() {
+  logger -s -t freifunk-berlin-migration $@
+}
+
+update_openvpn_remote_config() {
   # use dns instead of ips for vpn servers (introduced with 0.1.0)
+  log "Setting openvpn.ffvpn.remote to vpn03.berlin.freifunk.net"
   uci delete openvpn.ffvpn.remote
   uci add_list openvpn.ffvpn.remote='vpn03.berlin.freifunk.net 1194 udp'
   uci add_list openvpn.ffvpn.remote='vpn03-backup.berlin.freifunk.net 1194 udp'
 }
 
-function update_openvpn_lease_config() {
+update_dhcp_lease_config() {
   # set lease time to 5 minutes (introduced with 0.1.0)
-  uci set dhcp.dhcp.leasetime='5m'
+  local current_leasetime=$(uci get dhcp.dhcp.leasetime)
+  if [ "x${current_leasetime}" = "x" ]; then
+    log "Setting dhcp lease time to 5m"
+    uci set dhcp.dhcp.leasetime='5m'
+  fi
+  
 }
 
-function update_wireless_ht20_config() {
+update_wireless_ht20_config() {
   # set htmode to ht20 (introduced with 0.1.0)
-  uci set wireless.radio0.htmode='ht20'
-  if uci get wireless.radio1; then
-    uci set wireless.radio1.htmode='ht20'
+  log "Setting htmode to HT20 for radio0"
+  uci set wireless.radio0.htmode='HT20'
+  local radio1_present=$(uci get wireless.radio1.htmode)
+  # set htmode if radio1 is present
+  if [ "x${radio1_present}" != x ]; then
+    log "Setting htmode to HT20 for radio1."
+    uci set wireless.radio1.htmode='HT20'
   fi
 }
 
-echo "Migrating from ${OLD_VERSION} to ${VERSION}."
+migrate () {
+  log "Migrating from ${OLD_VERSION} to ${VERSION}."
 
-if semverLT ${OLD_VERSION} "0.1.0"; then
+  if semverLT ${OLD_VERSION} "0.1.0"; then
+    update_openvpn_remote_config
+    update_dhcp_lease_config
+    update_wireless_ht20_config
+  fi
 
-  update_openvpn_remote_config
-  update_dhcp_lease_config
-  update_wireless_ht20_config
+  # overwrite version with the new version
+  log "Setting new system version to ${VERSION}."
+  uci set system.@system[0].version=${VERSION}
 
-fi
+  uci commit
 
-# set version old to current version after update
-# uci set system.version.version=${VERSION}
+  log "Migration done."
+}
 
-echo "Migration done."
+migrate
