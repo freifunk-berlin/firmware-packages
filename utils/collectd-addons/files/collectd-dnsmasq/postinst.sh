e6470f438a5587e90c97c5644b9d299c27aa2ff7
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-01-29 21:09:44 +0100

    collectd-snmp-templates: rename to "addons" and add "dnsmasq"
    * keep current name of "collectd-snmp-templates" package
      * separate directories for SNMP- and dnsmasq-stuff
    * add "collectd-dnsmasq-addon" package
      * "dnsmasq.sh" script run by collectd in background
      * runs in endless loop, to avoid spawning every time,
        which reduces the systemload
      * automatic setup of configuration in uci luci_statistics
        via opkg postinst-hook

---
+++ utils/collectd-addons/files/collectd-dnsmasq/postinst.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. /lib/functions.sh
+
+PLUGIN_SET=false
+
+is_dnsmasq() {
+  config_get script $1 cmdline
+  [ $script = '/etc/collectd/dnsmasq.sh' ] && PLUGIN_SET=true
+}
+
+config_load luci_statistics
+config_foreach is_dnsmasq collectd_exec_input
+
+[ $PLUGIN_SET  = true ] && exit 0
+
+cat >>/etc/config/luci_statistics <<EOF
+config collectd_exec_input
+        option cmdline '/etc/collectd/dnsmasq.sh'
+        option cmduser 'dnsmasq'
+        option cmdgroup 'dnsmasq'
+
+EOF
