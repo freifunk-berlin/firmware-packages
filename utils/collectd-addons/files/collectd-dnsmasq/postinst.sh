f68c68d8c980ed6d7d723536f330dd203b986cf9
Sven Roederer <SvenRoederer@users.noreply.github.com>
2017-02-19 20:56:56 +0100

    collectd-addons/dnsmasq: enable exec_plugin
    it's disabled by default after installation
    see openwrt-packages commit https://github.com/openwrt/packages/commit/2fc5a82b48cf124b7825a8bb3a1dd579a1e96629

--- utils/collectd-addons/files/collectd-dnsmasq/postinst.sh
+++ utils/collectd-addons/files/collectd-dnsmasq/postinst.sh
@@ -9,6 +9,12 @@ is_dnsmasq() {
   [ $script = '/etc/collectd/dnsmasq.sh' ] && PLUGIN_SET=true
 }
 
+# enable exec-plugin
+uci -m import luci_statistics <<EOF
+config statistics 'collectd_exec'
+  option enable '1'
+EOF
+
 config_load luci_statistics
 config_foreach is_dnsmasq collectd_exec_input
 
