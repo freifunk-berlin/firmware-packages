9a0eac53e05abb2ecd78d795999b35d888820984
Sven Roederer <SvenRoederer@users.noreply.github.com>
2017-02-19 20:57:47 +0100

    collectd-addons/dnsmasq: always rewrite addon-config
    this ensures to have the most recent settings

--- utils/collectd-addons/files/collectd-dnsmasq/postinst.sh
+++ utils/collectd-addons/files/collectd-dnsmasq/postinst.sh
@@ -2,28 +2,29 @@
 
 . /lib/functions.sh
 
-PLUGIN_SET=false
+ADDON_SECTIONS=''
 
-is_dnsmasq() {
+find_dnsmasq_addon() {
   config_get script $1 cmdline
-  [ $script = '/etc/collectd/dnsmasq.sh' ] && PLUGIN_SET=true
+  [ $script = '/etc/collectd/dnsmasq.sh' ] && ADDON_SECTIONS="${ADDON_SECTIONS} $1"
 }
 
-# enable exec-plugin
+# remove old addon-config(s)
+config_load luci_statistics
+config_foreach find_dnsmasq_addon collectd_exec_input
+for section in ${ADDON_SECTIONS}; do 
+  uci delete luci_statistics.$section
+done
+
+# add updated addon-config and enable exec-plugin
 uci -m import luci_statistics <<EOF
 config statistics 'collectd_exec'
-  option enable '1'
-EOF
+        option enable '1'
 
-config_load luci_statistics
-config_foreach is_dnsmasq collectd_exec_input
-
-[ $PLUGIN_SET  = true ] && exit 0
-
-cat >>/etc/config/luci_statistics <<EOF
 config collectd_exec_input
         option cmdline '/etc/collectd/dnsmasq.sh'
         option cmduser 'nobody'
         option cmdgroup 'nogroup'
-
 EOF
+
+uci commit luci_statistics
