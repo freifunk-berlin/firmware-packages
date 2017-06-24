41ea9464b98fa94638a9bd9c84446aa32a9970c7
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-06-24 19:16:18 +0200

    guard: add function "rename_guard <src> <dest>"

--- addons/freifunk-berlin-lib-guard/files/lib/functions/guard.sh
+++ addons/freifunk-berlin-lib-guard/files/lib/functions/guard.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
+CFG="defaults"
+CFG_FILE="/etc/config/$CFG"
+SECTION="packages"
+NAME="default"
+
 # check if defaults have already been set
 guard() {
-  CFG="defaults"
-  CFG_FILE="/etc/config/$CFG"
-  SECTION="packages"
-  NAME="default"
   OPTION=$1
 
   [ ! -f $CFG_FILE ] && touch $CFG_FILE && uci set $CFG.$NAME=$SECTION
   [ "$(uci -q get $CFG.$NAME.$OPTION)" == "1" ] && exit 0
   uci set $CFG.$NAME.$OPTION=1
+  uci commit $CFG
+}
+
+guard_rename() {
+  SRC=$1
+  DEST=$2
+
+  # get current setting or exit when not defined
+  cur_set=$(uci -q get $CFG.$NAME.$SRC) || return 0
+  uci delete $CFG.$NAME.$SRC
+  uci set $CFG.$NAME.$DEST=$cur_set
   uci commit $CFG
 }
