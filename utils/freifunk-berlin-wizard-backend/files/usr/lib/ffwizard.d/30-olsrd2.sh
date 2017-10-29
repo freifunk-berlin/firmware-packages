67d1eed7c65c0d1747624f02657c94e79b2b85d5
André Gaul <andre@gaul.io>
2017-10-29 14:56:22 +0100

    wizard-backend: add loopback to olsrd2

--- utils/freifunk-berlin-wizard-backend/files/usr/lib/ffwizard.d/30-olsrd2.sh
+++ utils/freifunk-berlin-wizard-backend/files/usr/lib/ffwizard.d/30-olsrd2.sh
@@ -27,6 +27,13 @@ EOF
   DOMAIN="$(uci add olsrd2 domain)"
   uci set olsrd2.$DOMAIN.table=51
 
+  INTERFACE="$(uci add olsrd2 interface)"
+  uci set olsrd2.$INTERFACE.ifname=loopback
+  uci set olsrd2.$INTERFACE.ignore=0
+  uci add_list olsrd2.$INTERFACE.bindto=-0.0.0.0/0
+  uci add_list olsrd2.$INTERFACE.bindto=-::1
+  uci add_list olsrd2.$INTERFACE.bindto=default_accept
+
   # add lan interface if meshLan is true
   local meshLan=$(echo $CONFIG_JSON | jsonfilter -e '@.ip.meshLan')
   if [ "$meshLan" == "true" ]; then
