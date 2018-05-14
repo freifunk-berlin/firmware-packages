7d36e61dbacd673bd267ab5e0d81fe5d7fdc393d
pmelange <isprotejesvalkata@gmail.com>
2018-05-15 01:23:15 +0200

    postinst.sh simplified and optimized for the future

--- addons/bbbdigger/files/postinst.sh
+++ addons/bbbdigger/files/postinst.sh
@@ -1,72 +1,80 @@
 #!/bin/sh
+#
+# As part of the install, we don't want to smash an already set up config
+# but at the same time update it if necessary.  The unique attributes are:
+#
+# network.bbbdigger_dev.macaddr
+# tunneldigger.bbbdigger.uuid
+#
+# All other config sections are overwritten with current settings
 
 . /lib/functions.sh
 
-BBBDIGGER_SERV='77.87.51.51:8942'
+TUNNEL_SERV='77.87.51.51:8942'
+IFACE=bbbdigger
 
 # tunneldigger UUID (and MAC) generation, if there isn't one already
-UUID=$(uci get tunneldigger.bbbdigger.uuid)
+MAC=$(uci -q get network.${IFACE}_dev.macaddr)
 if [ $? -eq 1 ]; then
-  UUID="b6"
+  # start with b6 for Berliner 6ackbone
+  MAC="b6"
   for byte in 2 3 4 5 6; do
+    MAC=$MAC`dd if=/dev/urandom bs=1 count=1 2> /dev/null | hexdump -e '1/1 ":%02x"'`
+  done
+fi
+
+UUID=$(uci -q get tunneldigger.${IFACE}.uuid)
+if [ $? -eq 1 ]; then
+  UUID=$MAC
+  for byte in 7 8 9 10; do
     UUID=$UUID`dd if=/dev/urandom bs=1 count=1 2> /dev/null | hexdump -e '1/1 ":%02x"'`
   done
-
 fi
-echo $UUID
-
-# remove all existing bbbdigger refences from the config files
-uci delete tunneldigger.bbbdigger
-uci delete network.bbbdigger
-uci delete network.bbbdigger_dev
-uci delete network.olsr_tunnel_bbbdigger_ipv4
-uci delete network.olsr_default_bbbdigger_ipv4
-uci delete network.olsr_default_unreachable_bbbdigger_ipv4
-ZONE=$(uci show firewall.zone_freifunk.network | cut -d \' -f 2 | sed 's/ *bbbdigger *//g')
-uci set firewall.zone_freifunk.network="${ZONE}"
-uci delete $(uci show olsrd | grep bbbdigger | cut -d = -f 1)
-
-#uci changes
 
 # tunneldigger setup
-uci set tunneldigger.bbbdigger=broker
-uci add_list tunneldigger.bbbdigger.address=$BBBDIGGER_SERV
-uci set tunneldigger.bbbdigger.uuid=$UUID
-uci set tunneldigger.bbbdigger.interface=bbbvpn
-uci set tunneldigger.bbbdigger.broker_selection=usage
-uci set tunneldigger.bbbdigger.enabled=1
+uci set tunneldigger.$IFACE=broker
+uci -q delete tunneldigger.$IFACE.address
+uci add_list tunneldigger.$IFACE.address=$TUNNEL_SERV
+uci set tunneldigger.$IFACE.uuid=$UUID
+uci set tunneldigger.$IFACE.interface=$IFACE
+uci set tunneldigger.$IFACE.broker_selection=usage
+uci set tunneldigger.$IFACE.enabled=1
 
 # network setup
-uci set network.bbbdigger_dev=device
-uci set network.bbbdigger_dev.macaddr=$UUID
-uci set network.bbbdigger_dev.name=bbbdigger
-uci set network.bbbdigger=interface
-uci set network.bbbdigger.proto=dhcp
-uci set network.bbbdigger.ifname=bbbdigger
+uci set network.${IFACE}_dev=device
+uci set network.${IFACE}_dev.macaddr=$MAC
+uci set network.${IFACE}_dev.name=$IFACE
 
-uci set network.olsr_tunnel_bbbdigger_ipv4=rule
-uci set network.olsr_tunnel_bbbdigger_ipv4.lookup=olsr-tunnel
-uci set network.olsr_tunnel_bbbdigger_ipv4.priority=19999
-uci set network.olsr_tunnel_bbbdigger_ipv4.in=bbbdigger
+uci set network.$IFACE=interface
+uci set network.$IFACE.proto=dhcp
+uci set network.$IFACE.ifname=$IFACE
 
-uci set network.olsr_default_bbbdigger_ipv4=rule
-uci set network.olsr_default_bbbdigger_ipv4.lookup=olsr-default
-uci set network.olsr_default_bbbdigger_ipv4.priority=20000
-uci set network.olsr_default_bbbdigger_ipv4.in=bbbdigger
+uci set network.olsr_tunnel_$IFACE_ipv4=rule
+uci set network.olsr_tunnel_$IFACE_ipv4.lookup=olsr-tunnel
+uci set network.olsr_tunnel_$IFACE_ipv4.priority=19999
+uci set network.olsr_tunnel_$IFACE_ipv4.in=$IFACE
 
-uci set network.olsr_default_unreachable_bbbdigger_ipv4=rule
-uci set network.olsr_default_unreachable_bbbdigger_ipv4.action=unreachable
-uci set network.olsr_default_unreachable_bbbdigger_ipv4.priority=20001
-uci set network.olsr_default_unreachable_bbbdigger_ipv4.in=bbbdigger
+uci set network.olsr_default_$IFACE_ipv4=rule
+uci set network.olsr_default_$IFACE_ipv4.lookup=olsr-default
+uci set network.olsr_default_$IFACE_ipv4.priority=20000
+uci set network.olsr_default_$IFACE_ipv4.in=$IFACE
+
+uci set network.olsr_default_unreachable_$IFACE_ipv4=rule
+uci set network.olsr_default_unreachable_$IFACE_ipv4.action=unreachable
+uci set network.olsr_default_unreachable_$IFACE_ipv4.priority=20001
+uci set network.olsr_default_unreachable_$IFACE_ipv4.in=$IFACE
  
-# firewall setup
-ZONE="$ZONE bbbdigger"
+# firewall setup (first remove from the zone and add it back)
+ZONE=$(echo $(uci show firewall.zone_freifunk.network | cut -d \' -f 2 | sed "s/${IFACE}//g"))
+ZONE="$ZONE $IFACE"
 uci set firewall.zone_freifunk.network="${ZONE}"
 
-# olsr setup
+# olsr setup (first remove it and add it again)
+SECTION=$(uci show olsrd | grep ${IFACE} | cut -d . -f 1-2)
+[ ! -z $SECTION ] && uci delete $SECTION
 uci add olsrd Interface
 uci set olsrd.@Interface[-1].ignore=0
-uci set olsrd.@Interface[-1].interface=bbbdigger
+uci set olsrd.@Interface[-1].interface=$IFACE
 uci set olsrd.@Interface[-1].Mode=ether
 
 #uci changes
