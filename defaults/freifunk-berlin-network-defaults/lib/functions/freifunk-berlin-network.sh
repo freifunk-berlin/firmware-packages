a3daee4a5b80b39fe86be67c6f552920c112192b
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-11-16 09:04:06 +0100

    network: use create_ffuplink() for initial setup of ffuplink-interface
    As create_ffuplink() can be called without having network.ffuplink
    defined during initial setup of node, we use "uci -q" to not display
    an errormessage

---
+++ defaults/freifunk-berlin-network-defaults/lib/functions/freifunk-berlin-network.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. /lib/functions/guard.sh
+
+create_ffuplink() {
+  curr=$(uci get ffberlin-uplink.preset.current)
+#  prev=$(uci get ffberlin-uplink.preset.previous)
+
+  uci -q delete network.ffuplink
+  # create a very basic ffuplink interface
+  uci set network.ffuplink=interface
+  uci set network.ffuplink.ifname=ffuplink
+  uci set network.ffuplink.ip4table=ffuplink
+  uci set network.ffuplink.ip6table=ffuplink
+  # the following options need to be set by the individual uplink-package
+  uci set network.ffuplink.proto=none
+  uci commit network.ffuplink
+
+  guard_delete notunnel
+  guard_delete tunnelberlin_openvpn
+  guard_delete tunnelberlin_tunneldigger
+  guard_delete vpn03_openvpn
+}
