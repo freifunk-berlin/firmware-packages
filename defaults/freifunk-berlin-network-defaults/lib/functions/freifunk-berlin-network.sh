0d88ebabcb10cb3fe5fbf066cbf754ce3738e380
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-11-16 09:04:06 +0100

    network: use create_ffuplink() for initial setup of ffuplink-interface

---
+++ defaults/freifunk-berlin-network-defaults/lib/functions/freifunk-berlin-network.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+. /lib/functions/guard.sh
+
+create_ffuplink() {
+  curr = $(uci get ffberlin-uplink.preset.current)
+  prev = $(uci get ffberlin-uplink.preset.previous)
+  #if [ $curr = 
+
+  uci delete network.ffuplink
+  # create a very basic ffuplink interface
+  uci set network.ffuplink=interface
+  uci set network.ffuplink.ifname=ffuplink
+  uci set network.ffuplink.ip4table=ffuplink
+  uci set network.ffuplink.ip6table=ffuplink
+  # the following options need to be set by the individual uplink-package
+  uci set network.ffuplink.proto=none
+  uci set network.ffuplink.disabled=1
+  uci commit network.ffuplink
+
+  guard_delete notunnel
+  guard_delete tunnelberlin_openvpn
+  guard_delete tunnelberlin_tunneldigger
+  guard_delete vpn03_openvpn
+}
