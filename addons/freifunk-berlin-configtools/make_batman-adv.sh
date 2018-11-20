69a13dfb11103e3e974cb8e259d9620e884e5766
Sven Roederer <SvenRoederer@users.noreply.github.com>
2018-11-20 02:57:39 +0100

    config-tools: add make_batman-adv

---
+++ addons/freifunk-berlin-configtools/make_batman-adv.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+. /lib/functions.sh
+
+create_interface() {
+  MASTERIF=$1
+  
+  uci set network.${MASTERIF}_bat=interface
+  uci set network.${MASTERIF}_bat.ifname='@${MASTERIF}'
+  uci set network.${MASTERIF}_bat.proto='batadv'
+  uci set network.${MASTERIF}_bat.mesh='bat0'
+  uci commit network.${MASTERIF}_bat
+}
+
+add_batman_iface() {
+  DSTBRIDGE=$1
+  
+  ifnames="$(uci get network.${DSTBRIDGE}.ifname)"
+  list_contains ${ifnames} bat0 || uci set network.dhcp.ifname=${ifnames} bat0
+}
+
