6da0d4e755942aeaf57622a15c1cf554d61541f6
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-12-15 23:05:58 +0100

    migration: run jffs-cleanup as very last uci-default script
    move the clean up of possible duplicates of rom-files into a separate
    uci-default script, which will run as very last.
    This prevents from problems caused by https://github.com/freifunk-berlin/firmware/issues/641,
    but keeps this functionality.
    
    backport ea6d78598a3edf4daca5c72208946364b7d1b136

--- utils/freifunk-berlin-migration/uci-defaults/freifunk-berlin-01-migration.sh
+++ utils/freifunk-berlin-migration/uci-defaults/freifunk-berlin-01-migration.sh
@@ -471,9 +471,6 @@ migrate () {
   uci commit
 
   log "Migration done."
-
-  # delete any overlay config files duplicated from romfs by sysupgrade - saves JFFS2 space
-  cd /overlay/upper/etc/config/ && for i in *; do [ -f "$i" ] && if cmp -s "$i" "/rom/etc/config/$i"; then rm -f "$i"; fi; done;
 }
 
 migrate
