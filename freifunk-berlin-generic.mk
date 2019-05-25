5d3a597e0d5a0c3f672b8b93088426b4ce0b47c9
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-05-25 16:52:12 +0200

    generic.mk: add local definition of PKG_VERSION
    this is a direct copy from luci-feeds luci.mk

--- freifunk-berlin-generic.mk
+++ freifunk-berlin-generic.mk
@@ -15,4 +15,21 @@ else
 #$(info found luci.mk at $(LUCIMKFILE))
 endif
 
+PKG_VERSION?=$(if $(DUMP),x,$(strip $(shell \
+	if svn info >/dev/null 2>/dev/null; then \
+		revision="svn-r$$(LC_ALL=C svn info | sed -ne 's/^Revision: //p')"; \
+	elif git log -1 >/dev/null 2>/dev/null; then \
+		revision="svn-r$$(LC_ALL=C git log -1 | sed -ne 's/.*git-svn-id: .*@\([0-9]\+\) .*/\1/p')"; \
+		if [ "$$revision" = "svn-r" ]; then \
+			set -- $$(git log -1 --format="%ct %h" --abbrev=7); \
+			secs="$$(($$1 % 86400))"; \
+			yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
+			revision="$$(printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2")"; \
+		fi; \
+	else \
+		revision="unknown"; \
+	fi; \
+	echo "$$revision" \
+)))
+
 include $(LUCIMKFILE)
