f3fa031ca2d605c434246817dc140e5068dd0541
pmelange <isprotejesvalkata@gmail.com>
2019-06-13 10:39:34 +0200

    migration: remove cleaning non-profile config files from jffs system
    The removal of non-profile config files breaks the ability to configure
    any remaining and as yet untouched configurations.  The result of trying
    to modify one of the config files removed in the /overlay filesystem
    results in ```uci: I/O error```
    
    This has been documented in https://github.com/freifunk-berlin/firmware/issues/641
    
    cherry-picked from 4bfd061d9273e4d3026be2ef80d0ade525f79a45

--- utils/freifunk-berlin-migration/uci-defaults/zz_freifunk-berlin-migration-clean_jffs.sh
+++ utils/freifunk-berlin-migration/uci-defaults/zz_freifunk-berlin-migration-clean_jffs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
-# delete any overlay config files duplicated from romfs by sysupgrade - saves JFFS2 space
-cd /overlay/upper/etc/config/ && for i in *; do [ -f "$i" ] && if cmp -s "$i" "/rom/etc/config/$i"; then rm -f "$i"; fi; done;
+# delete any overlay profile config files duplicated from romfs by 
+# sysupgrade - saves JFFS2 space
+cd /overlay/upper/etc/config/ && for i in profile_*; do [ -f "$i" ] && if cmp -s "$i" "/rom/etc/config/$i"; then rm -f "$i"; fi; done;
