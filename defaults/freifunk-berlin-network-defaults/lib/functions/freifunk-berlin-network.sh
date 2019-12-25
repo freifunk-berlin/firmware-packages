81bd8c410463f48b6f3b695ef77a7b27b173391e
Sven Roederer <freifunk@it-solutions.geroedel.de>
2019-12-25 14:32:42 +0100

    network-defaults: add generate_random_mac_hex() to freifunk-berlin-network.sh
    generate_random_mac_hex(prefix: 2byte)
     generates a random MAC-adress with the given 1-byte prefix. The result will
     be a string formatted as "xx:xx:xx:xx:xx:xx".

--- defaults/freifunk-berlin-network-defaults/lib/functions/freifunk-berlin-network.sh
+++ defaults/freifunk-berlin-network-defaults/lib/functions/freifunk-berlin-network.sh
@@ -21,3 +21,17 @@ create_ffuplink() {
   guard_delete tunnelberlin_tunneldigger
   guard_delete vpn03_openvpn
 }
+
+generate_random_mac_hex() {
+  local prefix=$1
+
+  [ -n "$prefix" ] || return
+  local macaddr
+  # Create a static macaddr starting with "prefix" for ffuplink devices
+  # See the website https://www.itwissen.info/MAC-Adresse-MAC-address.html
+  for byte in 2 3 4 5 6; do
+    macaddr=$macaddr`dd if=/dev/urandom bs=1 count=1 2> /dev/null | hexdump -e '1/1 ":%02x"'`
+  done
+
+  echo $prefix$macaddr
+}
